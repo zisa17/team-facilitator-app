16fed1880ac80d7c83fcc4cb60e46d41368ec1e7
Zia Sarshar <zia.sarshar@gmail.com>
2026-03-31 13:41:50 +0200

    Add 'X-Client-Info' to CORS allowed headers

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey, Prefer');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, apikey, Prefer, X-Client-Info');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
